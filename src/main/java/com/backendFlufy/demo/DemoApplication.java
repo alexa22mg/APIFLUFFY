0ade9aa09574422f839e4291ca401e5d9540bd9c
Alexandra Moya Gomez <alexa22mg@gmail.com>
2023-07-22 19:04:50 -0500

    v2- nombre en Swagger

--- src/main/java/com/backendFlufy/demo/DemoApplication.java
+++ src/main/java/com/backendFlufy/demo/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.backendFlufy.demo;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +13,19 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+     @Bean
+	public OpenAPI crarapi(){
+		return new OpenAPI()
+				.info(new Info()
+						.title("Ecommerce Para Mascotas Fluffy API")
+						.version("0.1")
+						.description("Proyecto Educativo")
+						.termsOfService("http://swagger.io/terms/")
+						.license(new License().name("Apache2.0").url("http://sprindoc.org"))
+				);
+	 }
+
+
 
 }
